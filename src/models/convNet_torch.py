06a8e5140d16c70d5ebafded3cf952ae4e1c327c
nipdep <nipun1deelaka@gmail.com>
2021-02-11 18:10:55 +0530

    Update: convNet_torch

--- src/models/convNet_torch.py
+++ src/models/convNet_torch.py
@@ -1,7 +1,9 @@
 # To add a new cell, type '# %%'
 # To add a new markdown cell, type '# %% [markdown]'
 # %%
+from tqdm.notebook import tqdm
 import pandas as pd 
+from time import sleep
 
 import torch
 import torch.nn as nn 
@@ -11,7 +13,8 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import  transforms
 
 # %%
-
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# %%
 class MNIST(Dataset):
     def __init__(self, src_path, transform=None):
         self.transform = transform
@@ -24,7 +27,7 @@ class MNIST(Dataset):
         self.X = torch.reshape(X,(-1, 1, 28 ,28)).float()
     
     def __len__(self):
-        #assert len(self.y) == self.X[0]
+        assert len(self.y) == self.X.shape[0]
         return len(self.y)
 
     def __getitem__(self, idx):
@@ -45,7 +48,8 @@ transformer = transforms.Compose([
 ])
 
 dataset = MNIST('../../data/datasets/train.csv')
-loader = DataLoader(dataset,batch_size=20, shuffle=True, num_workers=0)
+batch_size = 20
+loader = DataLoader(dataset,batch_size=batch_size, shuffle=True, num_workers=0)
 
 # %%
 class ConvNet(nn.Module):
@@ -66,35 +70,68 @@ class ConvNet(nn.Module):
         x = x.view(-1, 32*5*5)
         x = F.relu(self.flatten(x))
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x)) ## this is the output layer.
-        x = F.softmax(x)        ## this is the logits layer.
+        #x = F.relu(self.fc2(x)) ## this is the output layer.
+        #x = F.log_softmax(x)        ## this is the logits layer.
+        x = self.fc2(x)
         return x 
 
 model = ConvNet()
-model
+model = model.to(device)
 
 # %%
 
 criterion = nn.CrossEntropyLoss()
+#criterion = nn.NLLLoss()
 
 optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
 
 # %%
 
-epochs = 10
+epochs = 15
 for epoch in range(epochs):
+        for i, data in enumerate(loader):
+            inputs, labels = data 
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            optimizer.zero_grad()
 
-    for i, data in enumerate(loader):
-        inputs, labels = data 
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-        optimizer.zero_grad()
-
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        print(f'[{epoch+1} : {i+1}] loss : {loss.item()}')
+            if i % 100 == 0:
+                print(f'[{epoch+1} : {i+1}] loss : {loss.item()}')
 print('Finished Training')
 
+# %%
+
+epochs = 15
+for epoch in range(epochs):
+    with tqdm(loader, unit='batch', position=0, leave=True) as progress_bar:
+        for data in progress_bar:
+            progress_bar.set_description(f'Epoch {epoch}:')
+
+            inputs, labels = data 
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            optimizer.zero_grad()
+
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            correct = (torch.argmax(outputs, axis=1) == labels).sum().item()
+            accuracy = correct / batch_size
+            progress_bar.set_postfix(loss=loss.item(), accuracy=100. * accuracy)
+            #progress_bar.update()
+
+print('Finished Training')
+
+# %%
+
+torch.save(model.state_dict(), '../../data/models/convNet_torch.pt')
+# %%
+torch.save(loader, '../../data/models/dataloader.pth')
 # %%
